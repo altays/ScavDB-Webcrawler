f0f187dcee15d13e1a8523e48a55c5943bc4357e
Alex Taylor <altays633@gmail.com>
2020-11-02 12:15:24 -0500

    creating objects for pod data, creating mongo connection, and async function to parse feeds and insert into DB

--- bot.js
+++ bot.js
@@ -8,11 +8,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const url = process.env.MONGO_URL;
 
-// store all RSS feeds in an array
-let whamRSS = "https://feed.goshortwave.com/podcasts/wet-hot-american-moon-juice-XSIgTGPJa-XSNxHb/rss"
-let spooky = "https://pinecast.com/feed/spooky-spouses"
-let cantWait = "https://feed.goshortwave.com/podcasts/can-t-hardly-wait-minute-rtJyNsmsjfKqeODv/rss"
-let alabaster = "https://pinecast.com/feed/alabasters-haberdashery"
+// store all RSS feeds as objects with link to scav page
 
 // research how to loop over objects
 
@@ -20,51 +16,86 @@ let alabaster = "https://pinecast.com/feed/alabasters-haberdashery"
 // run the load script for each podcast
 
 
+let alabaster = {
+    rssFeed: "https://pinecast.com/feed/alabasters-haberdashery",
+    scavLink: "https://www.scavengersnetwork.com/alabaster"
+}
+
+let wham = {
+    rssFeed: "https://feed.goshortwave.com/podcasts/wet-hot-american-moon-juice-XSIgTGPJa-XSNxHb/rss" ,
+    scavLink: "https://www.scavengersnetwork.com/whamjuice"
+}
+
+let spookypod = {
+    rssFeed: "https://pinecast.com/feed/spooky-spouses",
+    scavLink: "https://www.scavengersnetwork.com/spookyspouses"
+}
+
+let cantWaitPod ={
+    rssFeed: "https://feed.goshortwave.com/podcasts/can-t-hardly-wait-minute-rtJyNsmsjfKqeODv/rss",
+    scavLink: "https://www.scavengersnetwork.com/jamesanderinminutemovies"
+}
+  
 // Use connect method to connect to the Server
-MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, async function(err, client) {
     const db = client.db(process.env.MONGO_DATABASE)
     const collection = db.collection(process.env.MONGO_COLLECTION)
+    try {
 
     assert.strictEqual(null, err);   
     console.log("Connected successfully to server");
 
-    // something async needs to go into here
-    collection.find({}).toArray( async function(err, docs) {
-        try {
-            await assert.strictEqual(err, null);
-            await console.log("Found the following records");
-            await console.log(docs);
+    let podArray = []
 
+    let docs = await Feed.load(cantWait).then( rss => {
+        let {items, title, description, link, category, image} = rss;
+
+        let podcastMetaData = {
+            name: rss.title,
+            imageURL: rss.image,
+            // description: rss.description.split("\n")[0],
+            scavLink: "https://www.scavengersnetwork.com/jamesanderinminutemovies"
+        }
+
+        for (let i = 0; i < items.length; i++) {
+            let entryObject = {
+                ...podcastMetaData,
+                episodeTitle: rss.items[i].title,
+                titleShort:  rss.items[i].title.split('\n')[0].slice(0,24),
+                episodeDescription:rss.items[i].description.split("\n")[0],
+                episodeNumber: i+1
+            }
+            podArray.push(entryObject)
+        }
+        return podArray
+
+    });
+
+    // collection.find({}).toArray( async function(err, docs) {
+    //     try {
+    //         await console.log("Found the following records");
+    //         await console.log(docs);
+          
+    //         client.close(function() {
+    //             console.log("close connection")
+    //         })
+    //     } catch {
+    //         console.log(err)
+    //     }
+        
+    //   });
+
+    collection.insertMany(docs)
+        .then( async function(result,err) {
+            console.log(result)
             client.close(function() {
                 console.log("close connection")
             })
-        } catch{
+        }).catch(
             console.log(err)
-        }
-        // callback(docs);
-      });
+    )
 
-
+    } catch {
+        console.log(err)
+    }
 })
-
-// // getting title, description, image
-// Feed.load(alabaster).then(rss => {
-//     let {items, title, description, link, category, image} = rss;
-    
-//     // use this metadata!
-//     let podcastMetaData = {
-//         title: rss.title,
-//         image: rss.image,
-//         description: rss.description.split("\n")[0],
-//         scavLink: ""
-//     }
-
-//     for (let i = 0; i < items.length; i++) {
-//         console.log(podcastMetaData)
-//         console.log(rss.items[i].title)
-//         console.log(rss.items[i].description.split("\n")[0])
-//         console.log(rss.items[i].title.split('\n')[0].slice(0,24))  
-//         // combine into one message, make API call
-//     }
-
-// });
